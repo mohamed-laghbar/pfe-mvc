fff5f55e828a8cc4ca510f55ec077c7c41489868
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-01 14:53:01 +0100

    add comparing section

--- views/home.php
+++ views/home.php
@@ -1,65 +1,78 @@
-<!-- Navigation-->
-
-<!-- Masthead-->
 <header class="masthead"></header>
-<!-- Services-->
-<section class="page-section bg-light" id="services">
-  <div class="container">
-    <div class="text-center">
-      <h2 class="section-heading p-3 text-secondary">
-        3 Steps to Your Home Security System
-      </h2>
-    </div>
-    <div class="row text-center d-flex justify-content-around">
-      <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
-        <i class="h1 text-info ri-shopping-cart-fill"></i>
-        <h5 class="my-3">Build your system</h5>
-        <p class="text-muted">
-          Find the alarm systems, security cameras, and other sensors you need
-          to keep your home safe. Order what you need now and expand your system
-          as you go.
-        </p>
-      </div>
-      <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
-      <i class="h1 text-info ri-home-gear-line"></i>
-        <h5 class="my-3">Install it in minutes</h5>
-        <p class="text-muted">
-          Easy set-up, guaranteed. Install your system in minutes without any
-          tools or prior expertise. Want more help? Opt for our professional
-          install service at checkout
-        </p>
-      </div>
-      <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
-        <i class="h1 text-info ri-shield-keyhole-line"></i>
-        </span>
-        <h5 class="my-3">Watch from anywhere</h5>
-        <p class="text-muted">
-          View your cameras and alerts from your phone, no matter where you are.
-          Plus, get instant notifications for smoke, water, glass break, and
-          burglaries.
-        </p>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- end services -->
+<div class="container">
 
-<!-- Why Why Us -->
-<section>
-<h2>Compare Your Security Options</h2>
-<div>
-  <div>
-    <P>Other DIY systems</P>
-    <P><span></span> 36 month contract, monitoring costs $37-$53/month1</P>
-    <P><span></span>  Hardwired, needs landline</P>
-    <P><span></span> Poor rating on TrustPilot2</P>
-  </div>
+  <!-- Services-->
+  <section class="page-section bg-light" id="services">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading p-3 text-secondary">
+          3 Steps to Your Home Security System
+        </h2>
+      </div>
+      <div class="row text-center d-flex justify-content-around">
+        <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
+          <i class="h1 text-info ri-shopping-cart-fill"></i>
+          <h5 class="my-3">Build your system</h5>
+          <p class="text-muted">
+            Find the alarm systems, security cameras, and other sensors you need
+            to keep your home safe. Order what you need now and expand your system
+            as you go.
+          </p>
+        </div>
+        <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
+          <i class="h1 text-info ri-home-gear-line"></i>
+          <h5 class="my-3">Install it in minutes</h5>
+          <p class="text-muted">
+            Easy set-up, guaranteed. Install your system in minutes without any
+            tools or prior expertise. Want more help? Opt for our professional
+            install service at checkout
+          </p>
+        </div>
+        <div class="col-md-3 shadow-sm p-3 m-2 bg-white">
+          <i class="h1 text-info ri-shield-keyhole-line"></i>
+          </span>
+          <h5 class="my-3">Watch from anywhere</h5>
+          <p class="text-muted">
+            View your cameras and alerts from your phone, no matter where you are.
+            Plus, get instant notifications for smoke, water, glass break, and
+            burglaries.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end services -->
+
+  <!-- Why Why Us -->
+    <h1 class="text-secondary text-center mb-3">Compare Your Security Options</h1>
+    <div class="row d-flex justify-content-around">
+      <div class="col-md-5 bg-white p-3">
+        <P class="text-white p-2 bg-secondary">Other DIY systems</P>
+        <div>
+          <P><span class="text-danger h4"><i class="fa-solid fa-circle-xmark"></i></span> 36 month contract, monitoring
+            costs $37-$53/month1</P>
+          <P><span class="text-danger h4"><i class="fa-solid fa-circle-xmark"></i></span> Hardwired, needs landline</P>
+          <P><span class="text-danger h4"><i class="fa-solid fa-circle-xmark"></i></span> Poor rating on TrustPilot2</P>
+        </div>
+      </div>
+
+      <div class="col-md-5 bg-white p-3">
+        <P class=" text-white p-2 bg-primary"><span class="text-dark">02</span><span>safe</span><span
+            class="text-dark">.com</span> System</P>
+        <div>
+          <P><span class="text-success h4"><i class="fa-solid fa-circle-check"></i></span> No contracts, No Monthly Fees
+          </P>
+          <P><span class="text-success h4"><i class="fa-solid fa-circle-check"></i></span> Wireless, no drilling or
+            landline required</P>
+          <P><span class="text-success h4"><i class="fa-solid fa-circle-check"></i></span> Easy to setup yourself in no
+            time</P>
+            <button class="btn btn-primary text-black">Protect your house now!</button>
+        </div>
+      </div>
+    </div>
+  <!-- end why us -->
+
 </div>
-
-</section>
-<!-- end why us -->
-
-
 
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
