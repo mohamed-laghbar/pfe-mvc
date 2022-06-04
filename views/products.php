5fbbab118765a1ab1b8d558b715ce8dc285bd7f2
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-04 18:13:00 +0100

    add page products

---
+++ views/products.php
@@ -0,0 +1,156 @@
+<section class="page-section bg-light" id="services">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading p-2 text-secondary">
+Intrusion        </h2>
+      </div>
+      <div class="row text-center d-flex justify-content-around">
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/g/l/glass_break_sensor1_borderless.png" alt="">
+         <h5 class="my-3"> Motion Sensor</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/g/a/garage_door_sensor1_borderless.png" alt="">
+         <h5 class="my-3">Glass Break Sensor</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2  bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/w/i/window_door_sensor1_borderless.png" alt="">
+         <h5 class="my-3">Door/ Window Sensor</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/c/a/carbon_monoxide1_borderless.png" alt="">
+         <h5 class="my-3">Carbon Monoxide Sensor</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading p-2 text-secondary">
+Cameras        </h2>
+      </div>
+      <div class="row text-center d-flex me-5 justify-content-around">
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/d/o/doorbell_camera_bronze1_borderless_1.png" alt="">
+         <h5 class="my-3"> Doorbell Camera</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/i/n/indoor_camera1_borderless.png" alt="">
+         <h5 class="my-3">Indoor Camera</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2  bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/7/2/724_outdoorcamera_0001_v724_outdoorcamera_070720_02_3_.png" alt="">
+         <h5 class="my-3">OutDoor Camera</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/p/r/premium_indoor_camera1_borderless.png" alt="">
+         <h5 class="my-3">Premium Indoor Camera</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading p-2 text-secondary">
+Home Autoumation        </h2>
+      </div>
+      <div class="row text-center d-flex justify-content-around">
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/o/u/outdoorplug_borderless_1.jpg" alt="">
+         <h5 class="my-3"> OutDoor Smart Plug</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/w/i/wireless_light_control1_borderless.png" alt="">
+         <h5 class="my-3">Light Control</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2  bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/t/h/thermostat1_borderless.png" alt="">
+         <h5 class="my-3">Smart Thermostat</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/d/o/doorlock_bronze1_borderless_1.png" alt="">
+         <h5 class="my-3">Smart DoorLock</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading p-2 text-secondary">
+02safe Systems        </h2>
+      </div>
+      <div class="row text-center d-flex justify-content-around">
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/t/o/touch_screen1_borderless.png" alt="">
+         <h5 class="my-3"> 02safe TouchScreen</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/k/e/keypad1_borderless.png" alt="">
+         <h5 class="my-3">Extra Keyboard</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+        <div class="col-md-3 shadow-sm p-2  bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/k/e/key_fob1_borderless.png" alt="">
+         <h5 class="my-3">KeyChain Remote</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+
+        <div class="col-md-3 shadow-sm p-2 bg-white">
+          </span>
+         <img src="https://www.frontpointsecurity.com/media/catalog/product/cache/853fc44df41be63fd083492ab92d5c46/h/u/hub_keypad3_borderless.png" alt="">
+         <h5 class="my-3">02safe Hub and Keypad</h5>
+         <p><a class="h6 text-success text-decoration-underline" href="">See Details</a></p>
+
+        </div>
+      </div>
+    </div>
+  </section>
