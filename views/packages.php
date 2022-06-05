d61b6132046af0716dce66a147f71fb31d1b7629
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-05 13:59:18 +0100

    add page packages

---
+++ views/packages.php
@@ -0,0 +1,157 @@
+<header class="masthead"></header>
+<section class="page-section bg-light" id="services">
+    <div class="container-fluid">
+      <div class="text-center">
+        
+      </div>
+      <div class="row  d-flex justify-content-center">
+      <div class="col-md-4 p-5 shadow-sm   bg-white" >
+          
+          <h2 class=" text-muted fw-light mb-5">The Haven</h2>
+          <p class="text-muted ">
+          Water damage and fires are some of the most frequent and expensive home insurance claims. The Haven has everything you need to stop them.
+          </p>
+            <p class="text-muted mb-1" ><span><i class="text-success fa-solid fa-check"></i></span> no contracts</p><br>
+            <p class="text-muted mb-1 "><span><i class="text-success fa-solid fa-check"></i></span> 24/7 alarm monitoring</p><br>
+            <p class="text-muted "><span><i class="text-success fa-solid fa-check"></i></span> no phoneline needed</p><br>
+            <div class="pricing mt-5 d-flex justify-content-evenly">
+                <div class="price"><p class="h3 text-red  fw-light">$439 <span class="oldprice"> $504.86</span></p></div>
+                <div ><a href="" class=" btn bg-primary text-white  btn-lg ">Add To Cart <span class="text-white"><i class="fa-solid fa-cart-plus"></i></span> </a> </div>
+            </div>
+        </div>
+
+       <div class="col-md-4  shadow-sm bg-white"  >
+        <img class="" src="https://images.ctfassets.net/br4ichkdqihc/1g0pvZ5CJCmtHArFoorhjE/bb6b735c4fe01ed4144f3fca73c0b81b/HAVEN-SQ.jpg?w=800&h=800&q=50&fm=webp" height="650px" alt="">            
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container-fluid">
+      <div class="text-center">
+        
+      </div>
+      <div class="row  d-flex justify-content-center">
+      <div class="col-md-4 p-5 shadow-sm   bg-white" >
+          
+          <h2 class=" text-muted fw-light mb-5">The Knox</h2>
+          <p class="text-muted ">
+          For large homes, look no further. This kit’s got twice the entry and motion protection of the Hearth. It’s a great fit for homes with four or more bedrooms.          </p>
+            <p class="text-muted mb-1" ><span><i class="text-success fa-solid fa-check"></i></span> no contracts</p><br>
+            <p class="text-muted mb-1 "><span><i class="text-success fa-solid fa-check"></i></span> 24/7 alarm monitoring</p><br>
+            <p class="text-muted "><span><i class="text-success fa-solid fa-check"></i></span> no phoneline needed</p><br>
+            <div class="pricing mt-5 d-flex justify-content-evenly">
+                <div class="price"><p class="h3 text-red  fw-light">$371 <span class="oldprice"> $561.86</span></p></div>
+                <div ><a href="" class=" btn bg-primary text-white btn-lg ">Add To Cart <span class="text-white"><i class="fa-solid fa-cart-plus"></i></span></a> </div>
+            </div>
+        </div>
+
+       <div class="col-md-4 shadow-sm bg-white" >
+        <img src="https://images.ctfassets.net/br4ichkdqihc/54E8HaRdRfJ2AfT0iAhvas/fc1073817633b783dca5bd2340012272/knox-13-w.jpg?w=800&h=640&q=50&fm=webp" width="650px" height="650px" alt="">            
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container-fluid">
+      <div class="text-center">
+        
+      </div>
+      <div class="row  d-flex justify-content-center">
+      <div class="col-md-4 p-5 shadow-sm   bg-white" >
+          
+          <h2 class=" text-muted fw-light mb-5">The Hearth</h2>
+          <p class="text-muted ">
+          All the protection of the Essentials, plus smoke detection. Not to mention a 105dB extra siren. It’s about as loud as front row at Zeppelin in ’72. Intruders, start running.          </p>
+            <p class="text-muted mb-1" ><span><i class="text-success fa-solid fa-check"></i></span> no contracts</p><br>
+            <p class="text-muted mb-1 "><span><i class="text-success fa-solid fa-check"></i></span> 24/7 alarm monitoring</p><br>
+            <p class="text-muted "><span><i class="text-success fa-solid fa-check"></i></span> no phoneline needed</p><br>
+            <div class="pricing mt-5 d-flex justify-content-evenly">
+                <div class="price"><p class="h3 text-red  fw-light">$299 <span class="oldprice"> $389.86</span></p></div>
+                <div ><a href="" class=" btn bg-primary text-white btn-lg  ">Add To Cart <span class="text-white"><i class="fa-solid fa-cart-plus"></i></span></a> </div>
+            </div>
+        </div>
+
+       <div class="col-md-4 shadow-sm bg-white" >
+        <img src="https://images.ctfassets.net/br4ichkdqihc/4CD7PFZJd0QbyOHofOK6nB/193f9a7f2081e86de1bd484490abb626/hearth-9-w.jpg?w=800&h=640&q=50&fm=webp" width="650px" height="650px" alt="">            
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container-fluid">
+      <div class="text-center">
+        
+      </div>
+      <div class="row  d-flex justify-content-center">
+      <div class="col-md-4 p-5 shadow-sm   bg-white" >
+          
+          <h2 class=" text-muted fw-light mb-5">The Essentials</h2>
+          <p class="text-muted ">
+          Our most popular system. For good reason. It’s got a motion sensor to catch anyone walking through a main passage, plus entry sensors to cover all your main entry points.          </p>
+            <p class="text-muted mb-1" ><span><i class="text-success fa-solid fa-check"></i></span> no contracts</p><br>
+            <p class="text-muted mb-1 "><span><i class="text-success fa-solid fa-check"></i></span> 24/7 alarm monitoring</p><br>
+            <p class="text-muted "><span><i class="text-success fa-solid fa-check"></i></span> no phoneline needed</p><br>
+            <div class="pricing mt-5 d-flex justify-content-evenly">
+                <div class="price"><p class="h3 text-red  fw-light">$219 <span class="oldprice"> $294.86</span></p></div>
+                <div ><a href="" class=" btn bg-primary text-white   btn-lg">Add To Cart <span class="text-white"><i class="fa-solid fa-cart-plus"></i></span></a> </div>
+            </div>
+        </div>
+
+       <div class="col-md-4 shadow-sm bg-white" >
+        <img src="https://images.ctfassets.net/br4ichkdqihc/2HlJaiXb03M9XJbE0CQDTS/0627f0a85e3e245fb61c31a6f377dabf/essentials-8-w.jpg?w=800&h=640&q=50&fm=webp" width="650px" height="650px" alt="">            
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <section class="page-section bg-light" id="services">
+    <div class="container-fluid">
+      <div class="text-center">
+        
+      </div>
+      <div class="row  d-flex justify-content-center">
+      <div class="col-md-4 p-5 shadow-sm   bg-white" >
+          
+          <h2 class=" text-muted fw-light mb-5">The Foundation
+</h2>
+          <p class="text-muted ">
+          We designed this kit for small homes and apartments. It’s got an entry sensor to cover your main door, plus a motion sensor for an extra layer of protection.          </p>
+            <p class="text-muted mb-1" ><span><i class="text-success fa-solid fa-check"></i></span> no contracts</p><br>
+            <p class="text-muted mb-1 "><span><i class="text-success fa-solid fa-check"></i></span> 24/7 alarm monitoring</p><br>
+            <p class="text-muted "><span><i class="text-success fa-solid fa-check"></i></span> no phoneline needed</p><br>
+            <div class="pricing mt-5 d-flex justify-content-evenly">
+                <div class="price"><p class="h3 text-red  fw-light ">$169 <span class="oldprice"> $219.86</span></p></div>
+                <div ><a href="" class=" btn bg-primary text-white btn-lg ">Add To Cart <span class="text-white"><i class="fa-solid fa-cart-plus"></i></span></a> </div>
+            </div>
+        </div>
+
+       <div class="col-md-4 shadow-sm bg-white" >
+        <img src="https://images.ctfassets.net/br4ichkdqihc/10w71bSIeVgaoIh6yXeizl/8a957789d211b7647babd1edc28bbdb1/foundation-4-w.jpg?w=800&h=640&q=50&fm=webp" width="650px" height="650px" alt="">            
+        </div>
+
+      </div>
+    </div>
+  </section>
