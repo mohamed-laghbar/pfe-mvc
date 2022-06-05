521b3c0c55e8c6e95196c0ee52591b73320499c4
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-05 18:49:38 +0100

    adding contact page

---
+++ views/contact.php
@@ -0,0 +1,53 @@
+<div class="mt-3 conatiner p-5  ">
+            <div class="text-center">
+                <h3 class="text-primary h1">How Can We Help You?</h3>
+                <p class="lead">You can expect a response within 3-5 business days.</p>
+            </div>
+            <div class=" d-flex align-items-center justify-content-center">
+                <div class="bg-white col-md-4">
+                    <div class="p-4  contact-shadow">
+
+                        <div>
+                        <div class="input-field mt-3">
+                    <span class="far fa-user p-2"></span>
+                    <input type="text" name="name" placeholder="Your Name" required class="">
+                   </div>
+
+                   <i class=""></i>
+
+                   <div class="input-field mt-3">
+                    <span class="far fa-envelope p-2"></span>
+                    <input type="text" name="email" placeholder="Email Address" required class="">
+                </div>
+
+
+
+
+
+                <div class="input-field mt-3">
+                    <span class="fa fa-share p-2"></span>
+                    <input type="text" name="subject"  placeholder="Subject"  class="">
+                </div>
+
+
+                
+                <div class="input-field mt-3">
+                    
+                    <textarea name="message" cols="20" rows="6" class="form-control" placeholder="Message"   >
+                    </textarea>
+                        
+                </div>
+
+                <input type="submit" name="submit" value="Send" class="btn btn-primary mt-3 ">
+                        
+
+
+                      
+                       
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
