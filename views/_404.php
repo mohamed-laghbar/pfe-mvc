7c10d4e1e0c9638de51b47583fd94933a9b348c9
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-06 15:04:27 +0100

    change to another  error 404 page

--- views/_404.php
+++ views/_404.php
@@ -1,11 +1,15 @@
-
 <div id="notfound">
 		<div class="notfound">
 			<div class="notfound-404">
-				<h3 class="bg-error ">Oops!</h3>
+				<h1>404</h1>
 			</div>
-			<h2 class="text-muted">404 - Page not found</h2>
-			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-			<a href="http://localhost:8080/">Go To Homepage</a>
+			<h2>Oops! Nothing was found</h2>
+			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable. <a href="http://localhost:8080/">Return to homepage</a></p>
+			<div class="notfound-social">
+				<a href="http://facebook.com"><i class="fa fa-facebook"></i></a>
+				<a href="http://twitter.com"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-pinterest"></i></a>
+				<a href="#"><i class="fa fa-google-plus"></i></a>
+			</div>
 		</div>
 	</div>
