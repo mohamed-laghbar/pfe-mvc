4f9204e7aa1f7b16807dd715fe1ad70c5cdf32f6
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-06 14:50:26 +0100

    adding error 404 page

--- views/_404.php
+++ views/_404.php
@@ -1,5 +1,11 @@
-<?php
 
-?>
-
-<h1>Not found</h1>
+<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h3 class="bg-error ">Oops!</h3>
+			</div>
+			<h2 class="text-muted">404 - Page not found</h2>
+			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+			<a href="http://localhost:8080/">Go To Homepage</a>
+		</div>
+	</div>
