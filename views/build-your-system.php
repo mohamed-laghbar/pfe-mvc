b900edc64431cfbf3fedd056713accc6a36db0ad
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-06 10:02:11 +0100

    finish page build your system

--- views/build-your-system.php
+++ views/build-your-system.php
@@ -35,7 +35,7 @@
 <section class="container">
 <p class="h3 fw-light">Choose your Burglary Sensors</p>
 <hr>
-<div class="row">
+<div class="row p-3">
     <div class="col-md-2"><img width="120px" src="https://images.ctfassets.net/br4ichkdqihc/37xKGlGGVZ6atMbOJ0TAh4/ecf83c3ba0491b35adc12e4df9348f46/entry_trans.png?w=387&h=352&q=50&fm=webp" alt=""></div>
     <div class="col-md-8">
         <p class="h4 fw-light">Entry Sensor</p>
@@ -44,11 +44,74 @@
 <b> Pro tip:</b> Stop burglars where they break in most. Ground floor doors and windows. For rooms with 3 or more windows, consider a Motion Sensor instead.</p></div>
     <div class="col-md-2">
         <p class="small-text fw-light">How many do you need</p>
-       <p class="text-primary fw-bold h4">$16.99</p> 
-       
+       <p class="text-muted fw-bold h4">$16.99</p> 
+       <input type="submit" class="btn btn-primary" value="Add To Cart">
     </div>
 
 </div>
+
+
+<hr>
+<div class="row p-3">
+    <div class="col-md-2"><img width="120px" src="https://images.ctfassets.net/br4ichkdqihc/7KgcRxPyGs7MZRn7pKaGCw/8203f46dd58ce83daab1eec22a0cd79b/motion_trans_clean.png?w=450&h=454&q=50&fm=webp" alt=""></div>
+    <div class="col-md-8">
+        <p class="h4 fw-light">Motion Sensor</p>
+    <p class="text-muted">Covers an entire room. Get at least one for a main room or hallway. Gets along great with pets.<br>
+
+<b> Pro tip:</b>Get one to protect rooms on the ground level with 3 or more windows. Skip getting entry sensors for these windows.</p></div>
+    <div class="col-md-2">
+        <p class="small-text fw-light">How many do you need</p>
+       <p class="text-muted fw-bold h4">$29.99</p> 
+       <input type="submit" class="btn btn-primary" value="Add To Cart">
+    </div>
+
+</div>
+
+
+
+
+<hr>
+<div class="row p-3">
+    <div class="col-md-2"><img width="120px" src="https://images.ctfassets.net/br4ichkdqihc/6nvQPmMqvjgK94VyvnajB4/d1405bdce3c55ac2f11b945a1958a8a2/20-0403-shield-955-shadow_2x.png?w=286&h=288&q=50&fm=webp" alt=""></div>
+    <div class="col-md-8">
+        <p class="h4 fw-light">Wireless Outdoor Camera</p>
+    <p class="text-muted">Protects your doors and windows. Works with most doors or windows. <br>
+
+<b> Pro tip:</b> Stop burglars where they break in most. Ground floor doors and windows. For rooms with 3 or more windows, consider a Motion Sensor instead.</p></div>
+    <div class="col-md-2">
+        <p class="small-text fw-light">How many do you need</p>
+       <p class="text-muted fw-bold h4">$169.99</p> 
+       <input type="submit" class="btn btn-primary" value="Add To Cart">
+    </div>
+
+</div>
+
+
+
+
+<hr>
+<div class="row p-3">
+    <div class="col-md-2"><img width="120px" src="https://images.ctfassets.net/br4ichkdqihc/3jNRxqEFt9PoWivL424jIy/8d18a5a0ecef740d06669f783c5ef90b/Camera_Transparent_sq.png?w=450&h=666&q=50&fm=webp" alt=""></div>
+    <div class="col-md-8">
+        <p class="h4 fw-light">Entry Sensor</p>
+    <p class="text-muted">Protects your doors and windows. Works with most doors or windows. <br>
+
+<b> Pro tip:</b> Stop burglars where they break in most. Ground floor doors and windows. For rooms with 3 or more windows, consider a Motion Sensor instead.</p></div>
+    <div class="col-md-2">
+        <p class="small-text fw-light">How many do you need</p>
+       <p class="text-muted fw-bold h4">$16.99</p> 
+       <input type="submit" class="btn btn-primary" value="Add To Cart">
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
 
 
 
