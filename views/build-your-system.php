9259fb186fe48a362ae2074b86bfe6d39978d1cb
Mohamed-Laghbar <simoghbaar@gmail.com>
2022-06-06 09:21:36 +0100

    add few section in page build your system

---
+++ views/build-your-system.php
@@ -0,0 +1,57 @@
+<div class="container-fluid mt-5">
+
+<div class="row p-5">
+    <div class="col-md-5 p-5">
+        <p class="h1 fw-light ">Personalize your 02safe System</p>
+        <p>Our security pros give you step-by-step advice on what sensors you'll need, starting with a Base Station and Keypad. Get your system in less than a week. We'll even pre-program it, so you can set it up in just a few minutes.</p>
+    </div>
+    <div class="col-md-6">
+        <div class="row  mb-4 ">
+            <div class="col-md-2">
+                <img height="170px" src="https://images.ctfassets.net/br4ichkdqihc/4EAnLHfReGdJXip7Y5S0na/17104d2a544c35a4471589c5856c7130/Base_Station__included__.png?w=249&h=534&q=50&fm=webp" alt="">
+            </div>
+            <div class="col-md-8">
+                <p class="h5">Base Station (included) <span class="bg-blue"><i class="fa-solid fa-circle-info"></i></span></p>
+                <p>The brains of the system. Built with a backup battery in case of a power outage. Just plug it in to start protecting.</p>
+            </div>
+        </div>
+
+        <div class="row  ">
+            <div class="col-md-2">
+                <img height="170px" w src="https://images.ctfassets.net/br4ichkdqihc/5NnVIxfKWETP41bVoiBdXv/ca51d1b2b88e9b5f043e0e339cb613bc/Wireless_Keypad__included_.png?w=222&h=456&q=50&fm=webp" alt="">
+            </div>
+            <div class="col-md-8">
+                <p class="h5">Base Station (included)  <span class="bg-blue"><i class="fa-solid fa-circle-info"></i></span></p>
+                <p>Arm your home with one touch. Plus, there's a hidden panic button in case of an emergency.</p>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+</div>
+
+
+<section class="container">
+<p class="h3 fw-light">Choose your Burglary Sensors</p>
+<hr>
+<div class="row">
+    <div class="col-md-2"><img width="120px" src="https://images.ctfassets.net/br4ichkdqihc/37xKGlGGVZ6atMbOJ0TAh4/ecf83c3ba0491b35adc12e4df9348f46/entry_trans.png?w=387&h=352&q=50&fm=webp" alt=""></div>
+    <div class="col-md-8">
+        <p class="h4 fw-light">Entry Sensor</p>
+    <p class="text-muted">Protects your doors and windows. Works with most doors or windows. <br>
+
+<b> Pro tip:</b> Stop burglars where they break in most. Ground floor doors and windows. For rooms with 3 or more windows, consider a Motion Sensor instead.</p></div>
+    <div class="col-md-2">
+        <p class="small-text fw-light">How many do you need</p>
+       <p class="text-primary fw-bold h4">$16.99</p> 
+       
+    </div>
+
+</div>
+
+
+
+</section>
+
+</div>
